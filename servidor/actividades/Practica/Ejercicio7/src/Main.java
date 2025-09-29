246034c6e0775353112f8c42f53487b0517f0906
Eduardo-1107 <eduardoarmas1107@gmail.com>
2025-09-29 12:55:47 +0100

    Completado Ejercicio 7

--- servidor/actividades/Practica/Ejercicio7/src/Main.java
+++ servidor/actividades/Practica/Ejercicio7/src/Main.java
@@ -1,18 +1,35 @@
 public class Main {
     static void main() {
-
-        String nombre = "Eduardo";
+        Funciones.contarVocales("Eduardo");
+        Funciones.ponerMayus("Hola soy Eduardo");
+    }
+    public class Funciones {
+        public static void contarVocales(String s) {
+        String nombre = s;
         nombre = nombre.toLowerCase();
         char letra;
         int cont = 0;
         char[] vocales = {'a','e','i','o','u'};
         for (int i = 0; i < nombre.length(); i++) {
             letra = nombre.charAt(i);
-            for (int j = 0; j < vocales.length; j++) {
-                if (letra == vocales[j])
+            for (char vocal : vocales) {
+                if (letra == vocal)
                     cont++;
             }
         }
         System.out.println("Existen " + cont + " vocales");
+        }
+        public static void ponerMayus(String str) {
+            String resultado = "";
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if (c >= 'a' && c <= 'z') {
+                    c = (char)(c - 32);
+                }
+                resultado += c;
+            }
+            System.out.println("Original: " + str);
+            System.out.println("Mayúsculas: " + resultado);
+        }
     }
 }
