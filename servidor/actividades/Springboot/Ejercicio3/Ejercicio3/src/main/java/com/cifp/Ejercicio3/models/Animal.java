99968aee70b848da01b1980e87ffb9d2288c8c58
Eduardo-1107 <eduardoarmas1107@gmail.com>
2025-10-13 17:05:04 +0100

    Añadidos getter y setters de Animal

--- servidor/actividades/Springboot/Ejercicio3/Ejercicio3/src/main/java/com/cifp/Ejercicio3/models/Animal.java
+++ servidor/actividades/Springboot/Ejercicio3/Ejercicio3/src/main/java/com/cifp/Ejercicio3/models/Animal.java
@@ -4,14 +4,42 @@ public class Animal {
 
     private int id;
     private String nombre;
-    private int vidaExtra;
+    private int vidaMedia;
     private boolean esExtinto;
 
     public Animal() {};
 
-    public Animal(int id, String nombre, int VidaExtra, boolean esExtinto) {
-
+    public Animal(int id, String nombre, int vidaMedia, boolean esExtinto) {
+        setId(id);
+        setNombre(nombre);
+        setVidaMedia(vidaMedia);
+        setEsExtinto(esExtinto);
     }
 
+    public int getId() {
+        return this.id;
+    }
+    public String getNombre() {
+        return this.nombre;
+    }
+    public int getVidaMedia() {
+        return this.vidaMedia;
+    }
+    public boolean getEsExtinto() {
+        return this.esExtinto;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void setVidaMedia(int vidaMedia) {
+        this.vidaMedia = vidaMedia;
+    }
+    public void setEsExtinto(boolean esExtinto) {
+        this.esExtinto = esExtinto;
+    }
 
 }
